07914d18d3b1f54f4a7ebd2c4e8a774f40f564f9
Timo Schneller <timo.schneller@hp.com>
2020-12-22 00:06:17 +0100

    fix: allow same key to be used multiple times, closes #1 (#2)

--- src/domHelpers.ts
+++ src/domHelpers.ts
@@ -1,12 +1,19 @@
 import { L10nCatalogMap, L10nElementMap, L10nValueMap } from './types';
 
 export function collectL10nElements(root: HTMLElement): L10nElementMap {
-  const l10nElementMap: L10nElementMap = new Map<string, HTMLElement>();
+  const l10nElementMap: L10nElementMap = new Map<string, HTMLElement[]>();
   const elementsWithL10nValue: NodeListOf<HTMLElement> = getL10nElements(root);
   elementsWithL10nValue.forEach((element: HTMLElement) => {
     const l10nKey: string | null = getL10nElementKey(element);
     if (l10nKey) {
-      l10nElementMap.set(l10nKey, element);
+      let elementsList: HTMLElement[];
+      if (l10nElementMap.has(l10nKey)) {
+        // @ts-ignore
+        elementsList = [...l10nElementMap.get(l10nKey), element]
+      } else {
+        elementsList = [element];
+      }
+      l10nElementMap.set(l10nKey, elementsList);
     } else {
       throw new Error('l10n key not defined at data-l10n attribute');
     }
@@ -19,9 +26,11 @@ export function exchangeL10nValues(l10nElements: L10nElementMap, l10nCatalogs: L
     // @ts-ignore
     const l10nCatalog: L10nValueMap = l10nCatalogs.get(language);
     l10nCatalog.forEach((translatedText: string, l10nKey: string) => {
-      const element: HTMLElement | undefined = l10nElements.get(l10nKey);
-      if (element) {
-        element.innerHTML = translatedText;
+      const elements: HTMLElement[] | undefined = l10nElements.get(l10nKey);
+      if (elements) {
+        elements.forEach((element: HTMLElement) => {
+          element.innerHTML = translatedText;
+        });
       }
     });
     return true;
